6163fec1f932250225312759761f08e2c5675edf
Rajat001 <singhrajat869@gmail.com>
2025-08-21 08:40:11 +0530

    New Seperate Folder For Java Interview Questions

--- src/main/java/com/codewithdurgesh/blog/javaInterviewQuestionPractice/PrintStream.java
+++ src/main/java/com/codewithdurgesh/blog/javaInterviewQuestionPractice/PrintStream.java
@@ -1,4 +1,9 @@
 package com.codewithdurgesh.blog.javaInterviewQuestionPractice;
 
+import java.io.*;
 public class PrintStream {
+
+    public static void main(String args[]){
+        System.out.println("Hello World I am here");
+    }
 }
